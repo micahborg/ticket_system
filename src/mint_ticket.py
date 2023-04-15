92a556b1e84500ef4b3409c7032601d6316615ac
Micah Borghese <micahborghese@gmail.com>
2023-04-15 15:29:11 -0500

    OOP Design for NFT

--- src/mint_ticket.py
+++ src/mint_ticket.py
@@ -10,21 +10,17 @@ JSON_RPC_URL = "https://s.altnet.rippletest.net:51234/"
 client = JsonRpcClient(JSON_RPC_URL)
 
 def main():
-    pass
-
-def get_wallet(seed):
-    # seed = "" # NEED TO FETCH the issuer account seed here
-    # generate seed from public and private keys
-    # but for testing, create a NEW wallet:
     issuer_wallet = xrpl.wallet.generate_faucet_wallet(client=client)
-    return issuer_wallet
+    issuer_address = issuer_wallet.classic_address
 
-def get_account_info(issuerAddr):
+    get_account_info(issuer_address)
+
+def get_account_info(issuer_address):
     # Look up info about your account
     from xrpl.models.requests.account_info import AccountInfo
 
     acct_info = AccountInfo(
-        account=issuerAddr,
+        account=issuer_address,
         ledger_index="validated",
         strict=True,
     )
@@ -33,13 +29,13 @@ def get_account_info(issuerAddr):
     result = response.result
     print("response.status: ", response.status)
     import json
-    print(json.dumps(response.result, indent=4, sort_keys=True)) # what do I wanna do with this?
+    print(json.dumps(result, indent=4, sort_keys=True)) # what do I wanna do with this?
 
-def issue_ticket():
+def issue_ticket(issuer_wallet, issuer_address):
     # Construct NFTokenMint transaction to mint 1 NFT
     print(f"Minting a NFT...")
     mint_tx = NFTokenMint(
-        account=issuerAddr,
+        account=issuer_address,
         nftoken_taxon=1,
         flags=NFTokenMintFlag.TF_TRANSFERABLE
     )
@@ -52,24 +48,29 @@ def issue_ticket():
     print(f"     Tx response: {mint_tx_result}")
 
 
+def print_nft_metadata(mint_tx_result):
+    # Print the NFT metadata
+    for node in mint_tx_result['meta']['AffectedNodes']:
+        if "CreatedNode" in list(node.keys())[0]:
+            print(f"\n - NFT metadata:"
+                f"\n        NFT ID: {node['CreatedNode']['NewFields']['NFTokens'][0]['NFToken']['NFTokenID']}"
+                f"\n  Raw metadata: {node}")
 
 
-for node in mint_tx_result['meta']['AffectedNodes']:
-    if "CreatedNode" in list(node.keys())[0]:
-        print(f"\n - NFT metadata:"
-              f"\n        NFT ID: {node['CreatedNode']['NewFields']['NFTokens'][0]['NFToken']['NFTokenID']}"
-              f"\n  Raw metadata: {node}")
+def get_account_nfts(issuer_address):
+    # Query the minted account for its NFTs
+    get_account_nfts = client.request(
+        AccountNFTs(account=issuer_address)
+    )
 
-# Query the minted account for its NFTs
-get_account_nfts = client.request(
-    AccountNFTs(account=issuerAddr)
-)
+    nft_int = 1
+    print(f"\n - NFTs owned by {issuer_address}:")
+    for nft in get_account_nfts.result['account_nfts']:
+        print(f"\n{nft_int}. NFToken metadata:"
+            f"\n    Issuer: {nft['Issuer']}"
+            f"\n    NFT ID: {nft['NFTokenID']}"
+            f"\n NFT Taxon: {nft['NFTokenTaxon']}")
+        nft_int += 1
 
-nft_int = 1
-print(f"\n - NFTs owned by {issuerAddr}:")
-for nft in get_account_nfts.result['account_nfts']:
-    print(f"\n{nft_int}. NFToken metadata:"
-          f"\n    Issuer: {nft['Issuer']}"
-          f"\n    NFT ID: {nft['NFTokenID']}"
-          f"\n NFT Taxon: {nft['NFTokenTaxon']}")
-    nft_int += 1
+if __name__ == "__main__":
+    main()
