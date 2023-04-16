49226f26492f0c0f58175c3ad4301b7e2d646309
Micah Borghese <micahborghese@gmail.com>
2023-04-15 23:43:37 -0500

    Updates

--- src/account.py
+++ src/account.py
@@ -4,8 +4,7 @@
 """
 
 from xrpl.transaction import safe_sign_and_autofill_transaction, send_reliable_submission
-from xrpl.models.transactions import Memo
-from xrpl.models.transactions.nftoken_mint import NFTokenMint, NFTokenMintFlag
+from xrpl.models.transactions.nftoken_mint import NFTokenMint
 from xrpl.models.requests import AccountNFTs
 from xrpl.models.requests.account_info import AccountInfo
 from xrpl.wallet import Wallet, generate_faucet_wallet
@@ -17,8 +16,8 @@ class Account:
     JSON_RPC_URL = "https://s.altnet.rippletest.net:51234/"
     client = JsonRpcClient(JSON_RPC_URL)
 
-    def __init__(self, seed=None):
-        if seed != None:
+    def __init__(self, seed=0):
+        if seed != 0:
             self.wallet = Wallet(seed=seed)
         else:
             self.wallet = generate_faucet_wallet(client=self.client)
@@ -74,3 +73,30 @@ class Account:
         #         f"\n       URI: {nft['URI']}")
         #     nft_int += 1
         return get_account_nfts.result['account_nfts']
+    
+class Buyer_Account(Account):
+    def __init__(self, seed=0):
+        super().__init__(seed)
+        self.is_seller = False
+
+class Seller_Account(Account):
+    def __init__(self, seed=0):
+        super().__init__(seed)
+        self.is_seller = True
+    
+    def sell_ticket(self, eventID: int, uuid: str):
+        # Construct NFTokenMint transaction to mint 1 NFT
+        print(f"Minting a NFT...")
+        mint_tx = NFTokenMint(
+            account=self.address,
+            nftoken_taxon=eventID,
+            uri=str_to_hex(uuid),
+        )
+        # Sign mint_tx using the issuer account
+        mint_tx_signed = safe_sign_and_autofill_transaction(transaction=mint_tx, wallet=self.wallet, client=self.client)
+        mint_tx_signed = send_reliable_submission(transaction=mint_tx_signed, client=self.client)
+        mint_tx_result = mint_tx_signed.result
+
+        print(f"\n  Mint tx result: {mint_tx_result['meta']['TransactionResult']}")
+        print(f"     Tx response: {mint_tx_result}")
+        return mint_tx_result
--- src/main.py
+++ src/main.py
@@ -1,18 +1,35 @@
 from account import Account
 
 def main():
-    # Create an account
-    account = Account()
-    print(f"Account address: {account.address}")
-    print(f"Account balance: {account.get_balance()}")
+    seller_account = seller_sign_in()
+    buyer_account = buyer_sign_in()
 
-    # Issue a ticket
+    print(f"seller_account: {seller_account.address}")
+    print(f"buyer_account: {buyer_account.address}")
+
     eventID = 1
     uuid = "1234567890"
-    account.issue_ticket(eventID, uuid)
 
-    # Get the NFTs
-    account.get_nfts()
+    seller_account.issue_ticket(eventID, uuid)
+    buyer_account.issue_ticket(eventID, uuid)
+
+def seller_sign_in(seed=0):
+    if seed != 0:
+        seller_account = Account(seed)
+    else:
+        seller_account = Account()
+    return seller_account
+
+def buyer_sign_in(seed=0):
+    if seed != 0:
+        buyer_account = Account(seed)
+    else:
+        buyer_account = Account()
+    return buyer_account
+
+def get_ticket(seller_account, buyer_account, eventID, uuid):
+    seller_account.issue_ticket(eventID, uuid)
+    buyer_account.issue_ticket(eventID, uuid)
 
 if __name__ == "__main__":
     main()
