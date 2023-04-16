58981a221cdcf1a51884e4a43ba595642c00bcdc
Micah Borghese <micahborghese@gmail.com>
2023-04-15 19:26:13 -0500

    Account.py updates

--- src/account.py
+++ src/account.py
@@ -6,13 +6,14 @@
 from xrpl.transaction import safe_sign_and_autofill_transaction, send_reliable_submission
 from xrpl.models.transactions.nftoken_mint import NFTokenMint, NFTokenMintFlag
 from xrpl.models.requests import AccountNFTs
-import xrpl.wallet
+from xrpl.models.requests.account_info import AccountInfo
+from xrpl.wallet import Wallet, generate_faucet_wallet
+from xrpl.utils import str_to_hex
 
 from xrpl.clients import JsonRpcClient # Define the network client
 
 def main():
     my_account = Account()
-    print("Hi")
     print(my_account.account_info())
     print(my_account.get_balance())
 
@@ -22,15 +23,13 @@ class Account:
 
     def __init__(self, seed=None):
         if seed != None:
-            self.issuer_wallet = xrpl.wallet.Wallet(seed=seed)
+            self.issuer_wallet = Wallet(seed=seed)
         else:
-            self.issuer_wallet = xrpl.wallet.generate_faucet_wallet(client=self.client)
+            self.issuer_wallet = generate_faucet_wallet(client=self.client)
         self.issuer_address = self.issuer_wallet.classic_address
 
     def account_info(self):
         # Look up info about your account
-        from xrpl.models.requests.account_info import AccountInfo
-
         acct_info = AccountInfo(
             account=self.issuer_address,
             ledger_index="validated",
@@ -46,16 +45,17 @@ class Account:
         result = self.account_info()
         return result['account_data']['Balance']
     
-    def issue_ticket(self):
+    def issue_ticket(self, eventID: int):
         # Construct NFTokenMint transaction to mint 1 NFT
         print(f"Minting a NFT...")
         mint_tx = NFTokenMint(
             account=self.issuer_address,
-            nftoken_taxon=1,
-            flags=NFTokenMintFlag.TF_TRANSFERABLE
+            nftoken_taxon=eventID,
+            
+
         )
         # Sign mint_tx using the issuer account
-        mint_tx_signed = safe_sign_and_autofill_transaction(transaction=mint_tx, wallet=self.issuer_wallet, client=client)
+        mint_tx_signed = safe_sign_and_autofill_transaction(transaction=mint_tx, wallet=self.issuer_wallet, client=self.client)
         mint_tx_signed = send_reliable_submission(transaction=mint_tx_signed, client=self.client)
         mint_tx_result = mint_tx_signed.result
 
@@ -66,7 +66,7 @@ class Account:
     def get_account_nfts(self):
         # Query the minted account for its NFTs
         get_account_nfts = self.client.request(
-            AccountNFTs(account=issuer_address)
+            AccountNFTs(account=self.issuer_address)
         )
 
         nft_int = 1
